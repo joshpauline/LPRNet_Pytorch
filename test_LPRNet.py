136a738b987115602fe15daa40bb2f2f00f348fd
Josh Pauline <joshpauline@Joshs-MacBook-Pro.local>
2021-02-19 13:44:39 +1100

    added files for training

--- test_LPRNet.py
+++ test_LPRNet.py
@@ -12,7 +12,7 @@ from model.LPRNet import build_lprnet
 # import torch.backends.cudnn as cudnn
 from torch.autograd import Variable
 import torch.nn.functional as F
-from torch.utils.data import *
+from torch.utils.data import BatchSampler, DataLoader
 from torch import optim
 import torch.nn as nn
 import numpy as np
@@ -28,10 +28,10 @@ def get_parser():
     parser.add_argument('--test_img_dirs', default="./data/test", help='the test images path')
     parser.add_argument('--dropout_rate', default=0, help='dropout rate.')
     parser.add_argument('--lpr_max_len', default=8, help='license plate number max length.')
-    parser.add_argument('--test_batch_size', default=100, help='testing batch size.')
+    parser.add_argument('--test_batch_size', default=50, help='testing batch size.')
     parser.add_argument('--phase_train', default=False, type=bool, help='train or test phase flag.')
     parser.add_argument('--num_workers', default=8, type=int, help='Number of workers used in dataloading')
-    parser.add_argument('--cuda', default=True, type=bool, help='Use cuda to train model')
+    parser.add_argument('--cuda', default=False, type=bool, help='Use cuda to train model')
     parser.add_argument('--show', default=False, type=bool, help='show test image and its predict result or not.')
     parser.add_argument('--pretrained_model', default='./weights/Final_LPRNet_model.pth', help='pretrained base model')
 
@@ -62,7 +62,7 @@ def test():
 
     # load pretrained model
     if args.pretrained_model:
-        lprnet.load_state_dict(torch.load(args.pretrained_model))
+        lprnet.load_state_dict(torch.load(args.pretrained_model, map_location=torch.device('cpu')))
         print("load pretrained model successful!")
     else:
         print("[Error] Can't found pretrained mode, please check!")
@@ -159,7 +159,7 @@ def show(img, label, target):
     img = cv2ImgAddText(img, lb, (0, 0))
     cv2.imshow("test", img)
     print("target: ", tg, " ### {} ### ".format(flag), "predict: ", lb)
-    cv2.waitKey()
+    cv2.waitKey(1)
     cv2.destroyAllWindows()
 
 def cv2ImgAddText(img, text, pos, textColor=(255, 0, 0), textSize=12):
